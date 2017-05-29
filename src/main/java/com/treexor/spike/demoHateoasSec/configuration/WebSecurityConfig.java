46e864148b73fc73c9324fa169c889fa6db1c43b
Rafael Blazquez <rafael.blazquez@tuguu.com>
2017-05-29 16:24:12 +0100

    WROKING

--- src/main/java/com/treexor/spike/demoHateoasSec/configuration/WebSecurityConfig.java
+++ src/main/java/com/treexor/spike/demoHateoasSec/configuration/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.treexor.spike.demoHateoasSec.security.RequestHeaderAuthenticationDeta
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
